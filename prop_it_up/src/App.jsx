d0e7e5c04008df8de208e7a6a86de10e7ad6adf3
bbabilya <bjbabilya@gmail.com>
2022-07-07 18:01:14 -0500

    commit for core assignment

--- prop_it_up/src/App.jsx
+++ prop_it_up/src/App.jsx
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import User from './components/ClassComponent';
-//initialize import of model from ClassComponent.jsx
+// initialize import of model from ClassComponent.jsx
+// this file is closest to a python model.
 
 function App() {
   return (
     <div className="App">
-      <User first_name = {"Jane"} last_name = {"Doe"} age = {"29"} hair_color = {"Pink"} />
-      <User first_name = {"John"} last_name = {"Doe"} age = {"33"} hair_color = {"Black"} />
-      <User first_name = {"Kaija"} last_name = {"Pendergast"} age = {"20"} hair_color = {"Red"} />
+      {/* create an instance of "user" with all fields you want. */}
+      <User first_name = {"Jane"} last_name = {"Doe"} age = {29} hair_color = {"Pink"} pronoun = {"her"} />
+      <User first_name = {"John"} last_name = {"Doe"} age = {33} hair_color = {"Black"} pronoun = {"his"} />
+      <User first_name = {"Kaija"} last_name = {"Pendergast"} age = {20} hair_color = {"Red"} pronoun={"their"} />
     </div>
   );
 }
--- prop_it_up/src/components/ClassComponent.jsx
+++ prop_it_up/src/components/ClassComponent.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
     
-    
+// construct a props function to change age value.
 class User extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            // Defines the current 'state' for the value you are changing.
+            age: props.age
+        };
+    }
+
+    incrementAge = () => {
+        this.setState({age: this.state.age + 1});
+        //set the state for the value you're changing, along with the parameter for the change. In this case, we are incrementing the age + 1.
+        console.log("This works:", this.state.age);
+    }
+
     render() {
-        const {first_name, last_name, age, hair_color} = this.props;
+        const {first_name, last_name, pronoun, hair_color} = this.props;
         return(
             <div>
                 <h3>{first_name} {last_name}</h3>
-                <p>{age}</p>
-                <p>{hair_color}</p>
-                <button>This is a Button</button>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {hair_color}</p>
+                <button onClick = {this.incrementAge}>It's {pronoun} Birthday!</button>
             </div>
         );
     }
