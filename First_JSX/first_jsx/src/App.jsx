002baea1ece19901f24bbf7dc2fc736242cac034
bbabilya <bjbabilya@gmail.com>
2022-07-07 13:42:04 -0500

    initial

---
+++ First_JSX/first_jsx/src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {    
+  render() {        
+      return (            
+        <>
+          <h1>Hello Dojo</h1>        
+          <h2>Things I need to do:</h2>
+          <ul>
+            <li>
+              Study!
+            </li>
+            <li>
+              Be nicer to myself!
+            </li>
+            <li>
+              Take time to breathe.  	(*꒦ິ꒳꒦ີ)
+            </li>
+          </ul>
+     
+        </>
+      );    
+  }
+}
+
+export default App;
+
